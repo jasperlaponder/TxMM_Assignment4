a8a162d15dd4e7c26fad02da2e8ad730555f0911
Jasper Laponder <j.laponder@science.ru.nl>
2024-11-14 14:01:05 +0100

    added ablation study

--- classification.py
+++ classification.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
@@ -32,36 +33,67 @@ def scale(X):
     scaler = StandardScaler()
     return scaler.fit_transform(X)
 
-def extract_features(df, count_vectorizer, tfidf_vectorizer):
-    count_vector = extract_count_vector(df['text'], count_vectorizer)
-    tfidf_vector = extract_tfidf_vector(df['text'], tfidf_vectorizer)
-    sent_len = df['text'].apply(avg_sent_len)
-    char_counts = extract_char_counts(df['text'])
-    whitespace_counts = extract_whitespace_counts(df['text'])
-    return np.hstack((count_vector, tfidf_vector, sent_len.values.reshape(-1, 1), char_counts, whitespace_counts.reshape(-1, 1)))
+def extract_features(df, count_vectorizer, tfidf_vectorizer, count_vector=True, tfidf_vector=True, sent_len=True, char_counts=True, whitespace_counts=True):
+    X_list = []
+    if count_vector:
+        X_list.append(extract_count_vector(df['text'], count_vectorizer))
+    if tfidf_vector:
+        X_list.append(extract_tfidf_vector(df['text'], tfidf_vectorizer))
+    if sent_len:
+        X_list.append(df['text'].apply(avg_sent_len).values.reshape(-1, 1))
+    if char_counts:
+        X_list.append(np.array(extract_char_counts(df['text'])))
+    if whitespace_counts:
+        X_list.append(extract_whitespace_counts(df['text']).reshape(-1, 1))
+    return np.hstack(X_list)
 
-count_vector = CountVectorizer(max_features=50).fit(traindf['text'])
-tf_idf_vector = TfidfVectorizer(max_features=50).fit(traindf['text'])
+def train_and_evaluate(dev=True, count_vector_used=True, tfidf_vector_used=True, sent_len=True, char_counts=True, whitespace_counts=True):
+    # Train classifier
+    count_vector = CountVectorizer(max_features=50).fit(traindf['text'])
+    tf_idf_vector = TfidfVectorizer(max_features=50).fit(traindf['text'])
 
-X_train = extract_features(traindf, count_vector, tf_idf_vector)
-X_train_scaled = scale(X_train)
-y_train = traindf['author']
+    X_train = extract_features(traindf, count_vector, tf_idf_vector, count_vector_used, tfidf_vector_used, sent_len, char_counts, whitespace_counts)
+    X_train_scaled = scale(X_train)
+    y_train = traindf['author']
 
-svm = SVC().fit(X_train_scaled, y_train)
+    svm = SVC().fit(X_train_scaled, y_train)
 
-X_dev = extract_features(devdf, count_vector, tf_idf_vector)
-X_dev_scaled = scale(X_dev)
-y_dev = devdf['author']
+    if dev:
+        # Evaluate on dev set
+        X_dev = extract_features(devdf, count_vector, tf_idf_vector, count_vector_used, tfidf_vector_used, sent_len, char_counts, whitespace_counts)
+        X_dev_scaled = scale(X_dev)
+        y_dev = devdf['author']
 
-y_pred_dev = svm.predict(X_dev_scaled)
-f1_dev = f1_score(y_dev, y_pred_dev, average='weighted')
-print(f'F1 Score on dev set: {f1_dev}')
+        y_pred_dev = svm.predict(X_dev_scaled)
+        f1_dev = f1_score(y_dev, y_pred_dev, average='weighted')
+        print(f"Evaluated on dev set with F1 score: {f1_dev}")
+        print(f"Amount of features: {X_dev.shape[1]}")
+        return(f1_dev)
 
-X_test = extract_features(testdf, count_vector, tf_idf_vector)
-X_test_scaled = scale(X_test)
-y_test = testdf['author']
+    else:
+        # Evaluate on test set
+        X_test = extract_features(testdf, count_vector, tf_idf_vector, count_vector_used, tfidf_vector_used, sent_len, char_counts, whitespace_counts)
+        X_test_scaled = scale(X_test)
+        y_test = testdf['author']
 
-y_pred_test = svm.predict(X_test_scaled)
-f1 = f1_score(y_test, y_pred_test, average='weighted')
-print(f'F1 Score on test set: {f1}')
-print(f'With number of features: {X_train.shape[1]}')
+        y_pred_test = svm.predict(X_test_scaled)
+        f1 = f1_score(y_test, y_pred_test, average='weighted')
+        print(f"Evaluated on test set with F1 score: {f1}")
+        print(f"Amount of features: {X_test.shape[1]}")
+        return(f1)
+
+def ablation_study():
+    f1_scores = []
+    f1_scores.append(train_and_evaluate())
+    f1_scores.append(train_and_evaluate(count_vector_used=False))
+    f1_scores.append(train_and_evaluate(tfidf_vector_used=False))
+    f1_scores.append(train_and_evaluate(sent_len=False))
+    f1_scores.append(train_and_evaluate(char_counts=False))
+    f1_scores.append(train_and_evaluate(whitespace_counts=False))
+    return f1_scores
+
+labels = ['All Features', 'Count Vector', 'TF-IDF Vector', 'Sentence Length', 'Character Counts', 'Whitespace Counts']
+plt.bar(labels, ablation_study())
+plt.title('Ablation Study')
+plt.ylabel('F1 score')
+plt.show()
